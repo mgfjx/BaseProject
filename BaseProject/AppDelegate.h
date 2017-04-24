c70564c3ef24e03ed3b9ce93941858a31fc85f9d
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2017-04-24 10:24:14 +0800

    Initial Commit

---
+++ BaseProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseProject
+//
+//  Created by mgfjx on 2017/4/24.
+//  Copyright © 2017年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaseProject/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BaseProject
+//
+//  Created by mgfjx on 2017/4/24.
+//  Copyright © 2017年 XXL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
